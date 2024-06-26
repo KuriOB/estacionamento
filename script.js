98f70d6e652c2b7c010f1b310929ae9512fbc36f
Kuri <159294753+KuriOB@users.noreply.github.com>
2024-06-26 01:41:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
-
+/*
 document.addEventListener("DOMContentLoaded", (event) => {
     let elementosVagas = document.querySelectorAll('#vaga')
+
     elementosVagas.forEach(element => {
    let vaga = element.querySelector('.disponivelVaga');
 
@@ -24,26 +25,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
             alert('A vaga só pode ser removida quando estiver no estado "livre".');
             } else{ element.remove() ;}
         });
-    /*let botaoAdd = document.getElementById('adicionador') ;
-    let vagas = 10;
-    let adicionarVaga = document.createElement('div');
-    let novasVagas = document.getElementById('vagas')
-    let referencia = document.getElementById('vaga')
-
-    botaoAdd.addEventListener('click', function() {
-    //adicionarVaga.setAtribute('id', 'vaga' + vagas)
-    adicionarVaga.innerHTML = `<div id="vaga" class="row mb-4 text-center">
-    <button class="col-2 themed-grid-col remover">remover</button>
-    <div class="col-2 themed-grid-col">${vagas}</div>
-    <div class="col-2 themed-grid-col">Tempo</div>
-    <div class="col-2 themed-grid-col">preço</div>
-    <button class="col-2 themed-grid-col disponivelVaga livre">livre</button>
-  </div>`;
-  novasVagas.insertBefore(adicionarVaga, referencia);
-        vagas++;
-        });*/
-
     });
 });
-
+*/
 
